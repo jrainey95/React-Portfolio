9ff4a9f8d37e0138011d735632eff1c15bf3ca3b
jrainey95 <jonathan.rainey5@gmail.com>
2023-08-21 10:04:08 -0700

    new side

--- src/components/layout/Index.jsx
+++ src/components/layout/Index.jsx
@@ -3,7 +3,16 @@ import Sidebar from '../sidebar/Index';
 import './index.scss';
 
 const Layout = () => {
-  return <Sidebar/>
+  return (
+    <div className='App'>
+  <Sidebar/>
+    <div className='page'>
+      <span className='tags top-tags'>&lt</span>
+    </div>
+
+
+  </div>
+  )
 }
 
 export default Layout;
--- src/components/sidebar/Index.jsx
+++ src/components/sidebar/Index.jsx
@@ -1,17 +1,17 @@
 import './index.scss'
 import { Link, NavLink } from 'react-router-dom'
-import LogoS from '../../assets/images/jrr.png'
+// import LogoS from '../../assets/images/jrr.png'
 import LogoSubtitle from '../../assets/images/rainey-logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () =>{
   return (
   <div className='nav-bar'>
     <Link className='logo' to='/'>
-      <img src={LogoS} alt='logo' />
-       <img className="sub-logo" src={LogoSubtitle} alt='slobodan' />
+      <img src={LogoSubtitle} alt='logo' />
+       {/* <img className="sub-logo" src={LogoSubtitle} alt='slobodan' /> */}
     </Link>
 
     <nav>
@@ -26,11 +26,20 @@ const Sidebar = () =>{
       <NavLink exact="true" activeClassName="active" className="contact-link" to="/contact">
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
+    </nav>
+<ul>
+  <li>
     <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/jon-rainey/">
       <FontAwesomeIcon icon={faLinkedin} color="4d4d4e" />
     </a>
-    </nav>
-
+    </li>
+    <li>
+    <a target="_blank" rel="noreferrer" href="https://github.com/jrainey95/React-Portfolio">
+      <FontAwesomeIcon icon={faGithub} color="4d4d4e" />
+    </a>
+    </li>
+    
+    </ul>
 
 
 
