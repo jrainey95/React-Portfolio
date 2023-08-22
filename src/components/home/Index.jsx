dd49dd4f1193b107d68a423624cf753b805348ff
jrainey95 <jonathan.rainey5@gmail.com>
2023-08-21 19:12:36 -0700

    new

--- src/components/home/Index.jsx
+++ src/components/home/Index.jsx
@@ -1,14 +1,19 @@
 import LogoTitle from '../../assets/images/rainey-logo.png';
 import { Link } from 'react-router-dom';
 import './index.scss';
+import { useState } from 'react';
 
 const Home = () => {
+  const [letterClass, setLetterClass] =useState('text-animate');
+  const nameArray = ['J', 'o', 'n', 'a', 't', 'h', 'a', 'n', ' ', 'R', 'a', 'i', 'n','e','y']
+  const jobArray = ['W','e','b', ' ','D','e','v','e','l','o','p','e','r'];
+
   return(
     <div className='container home-page'>
       <div className='text-zone'>
         <h1>Hello, <br/> I'm 
-        Jonathan 
-        <img src={LogoTitle} alt="developer" />
+        Jonathan Rainey
+        {/* <img src={LogoTitle} alt="developer" /> */}
         <br/>
         A Web Developer
         </h1>
@@ -21,3 +26,7 @@ const Home = () => {
 } 
 
 export default Home;
+
+
+
+
