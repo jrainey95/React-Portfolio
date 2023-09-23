c9a72631dbe4de2e7a8b8f8a8ee665f0c5855bdc
jrainey95 <jonathan.rainey5@gmail.com>
2023-09-22 22:00:02 -0700

    aboutme

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import {  Routes, Route } from "react-router-dom";
 // import Navbar from "./components/Nav/Navbar";
 import Layout from './components/layout/Index';
 import Home from './components/Home'
+import About from './components/About/Index';
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
     <Routes>
     <Route path="/" element={<Layout />}>
     <Route index element={<Home />}/>
+    <Route path="about" element={<About />} />
     </Route>
 
     </Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './App.scss'
-import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import reportWebVitals from './reportWebVitals'
+// import { BrowserRouter } from 'react-router-dom'
+// import App from './App'
+// import './index.css'
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//     ,
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals()
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter from 'react-router-dom'
+
+import './App.scss';
 import './index.css';
 
-import AboutMe from './pages/AboutMe.jsx';
-import Portfolio from './pages/Portfolio.jsx';
-import Contact from './pages/Contact.jsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: "",
-    children: [
-      {
-        index: true,
-        element: <AboutMe />,
-      },
-      {
-        path: "/porfolio",
-        element: <Portfolio />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-    
-    ],
-  },
-]);
-
-
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-   <BrowserRouter>
-   <App />
-   </BrowserRouter>
-    
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
